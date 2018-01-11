e9ac1a2c898461631c840bc51d81bac42e4069b3
Evgeniy Kulikov <im@kulikov.im>
2018-01-11 13:45:21 +0300

    * sample.sql (for tests) + circle-ci

---
+++ middlewares/auth/sample.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  username VARCHAR(32) NOT NULL UNIQUE,
+  password VARCHAR(64) NOT NULL,
+  created_at timestamp without time zone NOT NULL,
+  updated_at timestamp without time zone NOT NULL
+);
+
+COMMIT;
