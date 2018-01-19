2da21208199e6922aff0d7d0e34b0f6bddf7f465
Evgeniy Kulikov <im-kulikov@users.noreply.github.com>
2018-01-19 15:58:39 +0300

    [TS-157] Config loader and validator (#4)
    * [TS-157] Config loader and validator
    
    Can work with sources:
    - io.Reader
    - string (path to file)
    
    * - tests

---
+++ config/config.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"gopkg.in/go-playground/validator.v9"
+	"gopkg.in/yaml.v2"
+)
+
+var ErrUnknownSourceType = errors.New("unknown type")
+
+func Load(src, config interface{}) error {
+	switch t := src.(type) {
+	case io.Reader:
+		return readAndValidate(t, config)
+	case string:
+		file, err := os.Open(t)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		return readAndValidate(file, config)
+	default:
+		return ErrUnknownSourceType
+	}
+}
+
+func readAndValidate(reader io.Reader, config interface{}) error {
+	var (
+		err error
+		buf []byte
+	)
+
+	if buf, err = ioutil.ReadAll(reader); err != nil {
+		return err
+	}
+
+	if err = yaml.Unmarshal(buf, config); err != nil {
+		return err
+	}
+
+	return validator.New().Struct(config)
+}
---
+++ config/config_test.go
@@ -0,0 +1,148 @@
+package config
+
+import (
+	"bytes"
+	"os"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+type testConfig struct {
+	A int `yaml:"a" validate:"required,gt=0,eq=1"`
+	B int `yaml:"b" validate:"required,gt=0,eq=2"`
+	C int `yaml:"c" validate:"required,gt=0,eq=3"`
+	D int `yaml:"d" validate:"required,gt=0,eq=4"`
+}
+
+var stringReader = strings.NewReader(`
+b: -1
+c: -2
+d: -3
+`)
+
+func TestLoad(t *testing.T) {
+	t.Run("should work with empty", func(t *testing.T) {
+		var (
+			err  error
+			buf  = bytes.NewBuffer(nil)
+			conf testConfig
+		)
+
+		err = Load(buf, &conf)
+
+		if _, ok := err.(validator.ValidationErrors); !ok {
+			t.Fatal("")
+		}
+	})
+
+	t.Run("should fail on closed file", func(t *testing.T) {
+		var (
+			err  error
+			file *os.File
+			conf testConfig
+		)
+
+		if file, err = os.Open("config.fixture.yaml"); err != nil {
+			t.Fatal(err)
+		}
+
+		file.Close()
+
+		err = Load(file, &conf)
+		if assert.Error(t, err) {
+			assert.Contains(t, err.Error(), os.ErrClosed.Error())
+		}
+	})
+
+	t.Run("should fail on file not exists", func(t *testing.T) {
+		var (
+			err  error
+			conf testConfig
+		)
+
+		err = Load("config.unknown.yaml", &conf)
+		if assert.Error(t, err) {
+			assert.Contains(t, err.Error(), "no such file or directory")
+		}
+	})
+
+	t.Run("should fail on bad yaml", func(t *testing.T) {
+		var (
+			err  error
+			buf  = bytes.NewBufferString(`a.a.a. "test": "not yaml" \c\c\t}`)
+			conf testConfig
+		)
+
+		err = Load(buf, &conf)
+		if assert.Error(t, err) {
+			assert.Contains(t, err.Error(), "yaml: ")
+		}
+	})
+
+	t.Run("should work with string as path", func(t *testing.T) {
+		var (
+			path = "config.fixture.yaml"
+			conf testConfig
+		)
+
+		if err := Load(path, &conf); err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, testConfig{
+			A: 1,
+			B: 2,
+			C: 3,
+			D: 4,
+		}, conf)
+	})
+
+	t.Run("should work with io.Reader", func(t *testing.T) {
+		var (
+			err  error
+			file *os.File
+			conf testConfig
+		)
+
+		if file, err = os.Open("config.fixture.yaml"); err != nil {
+			t.Fatal(err)
+		}
+
+		if err = Load(file, &conf); err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, testConfig{
+			A: 1,
+			B: 2,
+			C: 3,
+			D: 4,
+		}, conf)
+	})
+
+	t.Run("should fail on unknown source", func(t *testing.T) {
+		var conf testConfig
+		assert.EqualError(t, Load(nil, &conf), ErrUnknownSourceType.Error())
+	})
+
+	t.Run("should validate structure", func(t *testing.T) {
+		var (
+			err         error
+			conf        testConfig
+			fieldsCount = reflect.ValueOf(conf).NumField()
+		)
+
+		err = Load(stringReader, &conf)
+
+		switch errFields := err.(type) {
+		case validator.ValidationErrors:
+			assert.Equal(t, fieldsCount, len(errFields))
+		default:
+			t.Fatal(err)
+		}
+	})
+}
