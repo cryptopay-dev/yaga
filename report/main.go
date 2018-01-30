59273be89248112700fa355f1b0bcb05af479e4d
Evgeniy Kulikov <im-kulikov@users.noreply.github.com>
2018-01-30 17:55:32 +0300

    [TS-167] Report package docs (#23)

--- report/main.go
+++ report/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cryptopay-dev/go-metrics"
 )
 
+// CaptureResponseTime used to track response timing
 func CaptureResponseTime(platform, action string, now time.Time) {
 	metrics.SendWithTags(metrics.M{
 		"duration_ns": time.Since(now).Seconds() * 1e3,
@@ -15,6 +16,7 @@ func CaptureResponseTime(platform, action string, now time.Time) {
 	}, "exchange_timings")
 }
 
+// CaptureMargin used to track margin for pair
 func CaptureMargin(platform, pair string, margin float64) {
 	metrics.SendWithTags(metrics.M{
 		"margin": margin,
