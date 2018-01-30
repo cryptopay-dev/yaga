2b909eab6d8652635d86a7e8f03c495cac43355d
Evgeniy Kulikov <im@kulikov.im>
2018-01-30 17:41:27 +0300

    [TS-167] Locker package docs

--- locker/options.go
+++ locker/options.go
@@ -5,13 +5,16 @@ import (
 	"github.com/go-redis/redis"
 )
 
+// Options for creating Locker instance
 type Options struct {
 	Redis  *redis.Client
 	Logger logger.Logger
 }
 
+// Option closure
 type Option func(*Options)
 
+// newOptions converts slice of closures to Options-field
 func newOptions(opts ...Option) Options {
 	var options Options
 	for _, o := range opts {
@@ -20,12 +23,14 @@ func newOptions(opts ...Option) Options {
 	return options
 }
 
+// Redis closure to set field in Options
 func Redis(r *redis.Client) Option {
 	return func(o *Options) {
 		o.Redis = r
 	}
 }
 
+// Logger closure to set field in Options
 func Logger(l logger.Logger) Option {
 	return func(o *Options) {
 		o.Logger = l
