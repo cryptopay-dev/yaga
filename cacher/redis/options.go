b2978c365bc6a0ce7936264472301841d0291aaa
Evgeniy Kulikov <im-kulikov@users.noreply.github.com>
2018-01-30 17:01:03 +0300

    [TS-167] Cacher package docs (#21)
    * [TS-167] Cacher package docs
    
    * small fix
    
    * small fix
    
    * small fix

--- cacher/redis/options.go
+++ cacher/redis/options.go
@@ -1,11 +1,13 @@
 package redis
 
+// Options for creating redis-cacher
 type Options struct {
 	Address  string
 	Password string
 	DB       int
 }
 
+// newOptions converts slice of closures to Options-field
 func newOptions(opts ...Option) Options {
 	var options Options
 	for _, o := range opts {
@@ -14,20 +16,24 @@ func newOptions(opts ...Option) Options {
 	return options
 }
 
+// Option closure
 type Option func(*Options)
 
+// Address closure to set field in Options
 func Address(addr string) Option {
 	return func(o *Options) {
 		o.Address = addr
 	}
 }
 
+// Password closure to set field in Options
 func Password(password string) Option {
 	return func(o *Options) {
 		o.Password = password
 	}
 }
 
+// DB closure to set field in Options
 func DB(db int) Option {
 	return func(o *Options) {
 		o.DB = db
