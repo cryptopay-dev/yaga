99c5099400549b7c6589d911ac44778d5e638228
Evgeniy Kulikov <im-kulikov@users.noreply.github.com>
2018-01-31 17:32:15 +0300

    [TS-166] Locker package usage example (#33)

---
+++ examples/locker/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/cryptopay-dev/yaga/locker"
+	"github.com/cryptopay-dev/yaga/logger/nop"
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	store := redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	lock := locker.New(
+		locker.Logger(nop.New()),
+		locker.Redis(store),
+	)
+
+	wg := sync.WaitGroup{}
+
+	for i := 0; i <= 10; i++ {
+		wg.Add(1)
+		go func(index int) {
+			lock.Run("my-key", time.Second*10, func() error {
+				// Do some thing ... //
+
+				// For example
+				fmt.Println("Step :", index)
+				wg.Done()
+
+				return nil
+			})
+		}(i)
+	}
+
+	wg.Wait()
+}
