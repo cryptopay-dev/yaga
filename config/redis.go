6bdd06e331f642c4483571271b955fe3224da7e6
Evgeniy Kulikov <im@kulikov.im>
2018-02-07 15:55:32 +0300

    - Database.Connect - Redis.Connect

--- config/redis.go
+++ config/redis.go
@@ -1,8 +1,28 @@
 package config
 
+import (
+	"github.com/go-redis/redis"
+)
+
 // Redis default configuration
 type Redis struct {
 	Address  string `yaml:"address" validate:"required"`
 	Password string `yaml:"password" validate:"required"`
 	Database int    `yaml:"database" validate:"required"`
+}
+
+// Connect to Redis and check connection:
+func (r Redis) Connect() (*redis.Client, error) {
+	cache := redis.NewClient(&redis.Options{
+		Addr:     r.Address,
+		Password: r.Password,
+		DB:       r.Database,
+	})
+
+	// Check redis connection:
+	if _, err := cache.Ping().Result(); err != nil {
+		return nil, err
+	}
+
+	return cache, nil
 }
