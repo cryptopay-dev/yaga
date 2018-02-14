bdd21ee0b5ca335e5e01e623b5c4f7f5ba89d57a
Evgeniy Kulikov <im@kulikov.im>
2018-02-14 18:44:43 +0300

    [TS-000] Collection-render helper

---
+++ helpers/collection/collection.go
@@ -0,0 +1,91 @@
+package collection
+
+import (
+	"net/http"
+
+	"github.com/cryptopay-dev/yaga/errors"
+	"github.com/cryptopay-dev/yaga/web"
+	"github.com/go-pg/pg/orm"
+)
+
+const defaultOrdersLimit = 25
+
+// Filter interface
+type Filter interface {
+	Pager() Pager
+	Apply(*orm.Query) error
+}
+
+// Pager is part of filter structure
+type Pager struct {
+	Offset int `query:"offset" form:"offset" json:"offset"`
+	Limit  int `query:"limit" form:"limit" json:"limit"`
+}
+
+// Pager returns Pager-struct
+func (p Pager) Pager() Pager {
+	var limit = defaultOrdersLimit
+
+	if p.Limit > 0 {
+		limit = p.Limit
+	}
+
+	return Pager{
+		Offset: p.Offset,
+		Limit:  limit,
+	}
+}
+
+// Collection response answer
+type Collection struct {
+	Total  int   `json:"total"`
+	Offset int   `json:"offset"`
+	Items  Items `json:"items"`
+}
+
+// ModelsFetcher closure to fetch and format Items
+type ModelsFetcher = func(*orm.Query) (Items, error)
+
+// Items is formatted response of models
+type Items = []interface{}
+
+// Options to Format list-response
+type Options struct {
+	Query   *orm.Query
+	Fetcher ModelsFetcher
+	Filter  Filter
+}
+
+// Format collection as response-answer (Collection)
+func Format(ctx web.Context, opts Options) error {
+	if opts.Query == nil || opts.Filter == nil || opts.Fetcher == nil {
+		return nil
+	}
+
+	var (
+		err      error
+		pager    = opts.Filter.Pager()
+		response Collection
+	)
+
+	// Pagination:
+	opts.Query.Limit(pager.Limit)
+	opts.Query.Offset(pager.Offset)
+
+	if err = opts.Filter.Apply(opts.Query); err != nil {
+		ctx.Logger().Error(err.Error())
+		return errors.NewError(http.StatusBadRequest, err.Error())
+	}
+
+	if response.Total, err = opts.Query.Count(); err != nil {
+		ctx.Logger().Error(err.Error())
+		return errors.NewError(http.StatusInternalServerError, err.Error())
+	}
+
+	if response.Items, err = opts.Fetcher(opts.Query); err != nil {
+		ctx.Logger().Error(err.Error())
+		return errors.NewError(http.StatusInternalServerError, err.Error())
+	}
+
+	return ctx.JSON(http.StatusOK, response)
+}
