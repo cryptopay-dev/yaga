bce5c14b703b19912163d4335d6cd571be9834bf
Evgeniy Kulikov <im-kulikov@users.noreply.github.com>
2018-02-22 13:59:05 +0300

    [FEATURE] PostgreSQL configuration (#60)

--- config/database.go
+++ config/database.go
@@ -9,7 +9,7 @@ type Database struct {
 	Address  string `yaml:"address" validate:"required"`
 	Database string `yaml:"database" validate:"required"`
 	User     string `yaml:"user" validate:"required"`
-	Password string `yaml:"password" validate:"required"`
+	Password string `yaml:"password"`
 }
 
 // Connect to PostgreSQL and check connection:
