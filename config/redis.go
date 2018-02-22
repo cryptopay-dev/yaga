d59aa9aeb285885596349d856e83e52c1e5e37f8
Evgeniy Kulikov <im@kulikov.im>
2018-02-22 13:48:57 +0300

    [FEATURE] Redis configuration

--- config/redis.go
+++ config/redis.go
@@ -1,22 +1,28 @@
 package config
 
 import (
+	"time"
+
 	"github.com/go-redis/redis"
 )
 
 // Redis default configuration
 type Redis struct {
-	Address  string `yaml:"address" validate:"required"`
-	Database int    `yaml:"database"`
-	Password string `yaml:"password"`
+	Address     string        `yaml:"address" validate:"required"`
+	Database    int           `yaml:"database"`
+	Password    string        `yaml:"password"`
+	PoolSize    int           `yaml:"pool_size" validate:"required,gte=0"`
+	PoolTimeout time.Duration `yaml:"pool_timeout" validate:"required,gte=0"`
 }
 
 // Connect to Redis and check connection:
 func (r Redis) Connect() (*redis.Client, error) {
 	cache := redis.NewClient(&redis.Options{
-		Addr:     r.Address,
-		Password: r.Password,
-		DB:       r.Database,
+		Addr:        r.Address,
+		Password:    r.Password,
+		DB:          r.Database,
+		PoolSize:    r.PoolSize,
+		PoolTimeout: r.PoolTimeout,
 	})
 
 	// Check redis connection:
