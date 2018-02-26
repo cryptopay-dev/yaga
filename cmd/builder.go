5a952bc147cbb488e2fdad5b50d18937479afdf9
Evgenii <lobodin@gmail.com>
2018-02-26 13:48:40 +0300

    [TS-217] fix project example path

--- cmd/builder.go
+++ cmd/builder.go
@@ -11,6 +11,8 @@ import (
 	"github.com/cryptopay-dev/yaga/cli"
 )
 
+const pathProjectExample = "github.com/cryptopay-dev/yaga/cmd/project_example"
+
 var (
 	projectName string
 	projectPath string
@@ -68,6 +70,11 @@ func projectBuilder(opts *cli.Options, dir string) (err error) {
 	}
 
 	paths := strings.Split(path, ":")
+	srcProjectPath, err := getPathProjectExample(paths)
+	if err != nil {
+		return err
+	}
+
 	validPath := false
 	for _, path = range paths {
 		path, _ = filepath.Abs(path + "/src")
@@ -81,6 +88,7 @@ func projectBuilder(opts *cli.Options, dir string) (err error) {
 	if !validPath {
 		return fmt.Errorf("workdir should be inside GOPATH")
 	}
+	fmt.Printf("Project example path: %s\n\n", pathProjectExample)
 	fmt.Printf("Project mame: %s\n", projectName)
 	fmt.Printf("Project full name: %s\n", projectPath)
 	fmt.Printf("Project path: %s\n", dir)
@@ -89,13 +97,38 @@ func projectBuilder(opts *cli.Options, dir string) (err error) {
 		return err
 	}
 
-	if err = copyFiles(dir, "./project_example/"); err != nil {
+	if err = copyFiles(dir, srcProjectPath+"/"); err != nil {
 		return err
 	}
 
-	parseGolangFiles(opts, dir, "github.com/cryptopay-dev/yaga/cmd/project_example", projectPath)
+	parseGolangFiles(opts, dir, pathProjectExample, projectPath)
 
 	fmt.Println("Done.")
 
 	return nil
+}
+
+func isDirAndExist(dir string) (bool, error) {
+	mode, err := os.Stat(dir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	return mode.IsDir(), nil
+}
+
+func getPathProjectExample(paths []string) (string, error) {
+	for _, path := range paths {
+		path, _ = filepath.Abs(filepath.Join(path, "src", pathProjectExample))
+		if ok, err := isDirAndExist(path); err != nil {
+			return "", err
+		} else if ok {
+			return path, nil
+		}
+	}
+
+	return "", fmt.Errorf("cannot found project example")
 }
