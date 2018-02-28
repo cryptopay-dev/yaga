13e750e2e98051ba08d693daa81135cc995a69f7
Evgeniy Kulikov <im@kulikov.im>
2018-02-28 15:54:28 +0300

    tests + strings.Builder

--- validate/format.go
+++ validate/format.go
@@ -1,6 +1,7 @@
 package validate
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -12,13 +13,33 @@ import (
 func CheckErrors(valErr error) (ok bool, err error) {
 	var fieldsErr validator.ValidationErrors
 	if fieldsErr, ok = valErr.(validator.ValidationErrors); ok {
-		var fields []string
-		for _, field := range fieldsErr {
-			fields = append(fields, strings.ToLower(
-				field.Field(),
-			))
+		var (
+			fields strings.Builder
+			count  = len(fieldsErr) - 1
+		)
+
+		if _, wErr := fields.WriteString("bad "); wErr != nil {
+			ok = false
+			return
 		}
-		err = errors.NewError(http.StatusBadRequest, "bad "+strings.Join(fields, ", "))
+
+		for i, field := range fieldsErr {
+			if _, wErr := fmt.Fprintf(&fields, "`%s`", strings.ToLower(
+				field.Field(),
+			)); wErr != nil {
+				ok = false
+				return
+			}
+
+			if i < count {
+				if wErr := fields.WriteByte(','); wErr != nil {
+					ok = false
+					return
+				}
+			}
+		}
+		err = errors.NewError(http.StatusBadRequest, fields.String())
 	}
+
 	return
 }
