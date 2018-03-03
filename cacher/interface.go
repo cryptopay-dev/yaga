78b433267433b7d9b9929b795a5110cc1a4f38bf
Evgeniy Kulikov <im@kulikov.im>
2018-03-03 20:58:51 +0300

    fix Exists

--- cacher/interface.go
+++ cacher/interface.go
@@ -10,5 +10,5 @@ type Cacher interface {
 	TTL(key string) (time.Duration, error)
 	Expire(key string, duration time.Duration) error
 	Del(key string) error
-	Exists(key string) (int64, error)
+	Exists(keys ...string) (int64, error)
 }
