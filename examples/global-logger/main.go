00466e757ba3db6072e391f7968f608e9c57df8b
Evgeniy Kulikov <im@kulikov.im>
2018-04-02 18:11:01 +0300

    [GO-000] Global logger
    - global logger
    - examples
    - dep ensure -update

---
+++ examples/global-logger/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/cryptopay-dev/yaga/logger/log"
+)
+
+func main() {
+	log.Info("all fine!")
+	log.Print("all fine!")
+	log.Debug("all fine!")
+	log.Warn("all fine!")
+	log.Error("all fine!")
+	log.Panic("all fine!")
+}
