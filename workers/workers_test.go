f01bc310f59f0943bf3669e8a3f5b9e6ed06541a
Evgeniy Kulikov <im@kulikov.im>
2018-04-04 12:57:34 +0300

    make test without delta

--- workers/workers_test.go
+++ workers/workers_test.go
@@ -11,73 +11,82 @@ import (
 	"go.uber.org/atomic"
 )
 
+func testSimple(t *testing.T) {
+	t.Parallel()
+
+	c, cancel := context.WithCancel(context.Background())
+
+	w := New(c)
+
+	//start1 := time.Now()
+	//start2 := time.Now()
+
+	i := atomic.NewInt64(0)
+
+	if err := w.Schedule(&ScheduleOptions{
+		Name:     "#1: 100 ms worker",
+		Schedule: DelaySchedule(time.Millisecond * 100),
+		Handler: func(ctx context.Context) error {
+			i.Inc()
+			return nil
+		},
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := w.Schedule(&ScheduleOptions{
+		Name:     "#2: 200 ms worker",
+		Schedule: DelaySchedule(time.Millisecond * 200),
+		Handler: func(ctx context.Context) error {
+			i.Inc()
+			return nil
+		},
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := w.Schedule(&ScheduleOptions{
+		Name:     "#3: 1 sec worker",
+		Schedule: DelaySchedule(time.Second),
+		Handler: func(ctx context.Context) error {
+			panic("test")
+		},
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := w.Schedule(&ScheduleOptions{
+		Name:     "#4: 10 sec worker",
+		Schedule: DelaySchedule(time.Second * 10),
+		Handler: func(ctx context.Context) error {
+			t.Fatal("must not be runned")
+			return nil
+		},
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	time.AfterFunc(time.Millisecond*450, cancel)
+	w.Start()
+
+	<-c.Done()
+
+	w.Stop()
+
+	assert.Equal(t, int64(6), i.Load())
+}
+
 func TestWorkers(t *testing.T) {
 	log.Init()
 
 	t.Run("simple test workers", func(t *testing.T) {
 		t.Parallel()
 
-		c, cancel := context.WithCancel(context.Background())
-
-		w := New(c)
-
-		//start1 := time.Now()
-		//start2 := time.Now()
-
-		i := atomic.NewInt64(0)
-
-		if err := w.Schedule(&ScheduleOptions{
-			Name:     "#1: 100 ms worker",
-			Schedule: DelaySchedule(time.Millisecond * 100),
-			Handler: func(ctx context.Context) error {
-				i.Inc()
-				return nil
-			},
-		}); err != nil {
-			t.Fatal(err)
+		for i := 0; i < 10; i++ {
+			t.Run("for-loop", func(t *testing.T) {
+				testSimple(t)
+			})
 		}
-
-		if err := w.Schedule(&ScheduleOptions{
-			Name:     "#2: 200 ms worker",
-			Schedule: DelaySchedule(time.Millisecond * 200),
-			Handler: func(ctx context.Context) error {
-				i.Inc()
-				return nil
-			},
-		}); err != nil {
-			t.Fatal(err)
-		}
-
-		if err := w.Schedule(&ScheduleOptions{
-			Name:     "#3: 1 sec worker",
-			Schedule: DelaySchedule(time.Second),
-			Handler: func(ctx context.Context) error {
-				panic("test")
-			},
-		}); err != nil {
-			t.Fatal(err)
-		}
-
-		if err := w.Schedule(&ScheduleOptions{
-			Name:     "#4: 10 sec worker",
-			Schedule: DelaySchedule(time.Second * 10),
-			Handler: func(ctx context.Context) error {
-				t.Fatal("must not be runned")
-				return nil
-			},
-		}); err != nil {
-			t.Fatal(err)
-		}
-
-		w.Start()
-
-		time.AfterFunc(time.Millisecond*500, cancel)
-
-		<-c.Done()
-
-		w.Stop()
-
-		assert.InDelta(t, int64(6), i.Load(), 1)
 	})
 
 	t.Run("high way to hell", func(t *testing.T) {
